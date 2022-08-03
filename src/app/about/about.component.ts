667cda755ce915e127506fa36972128f7312ba52
gonza7795 <gonza_7795@yahoo.com.ar>
2022-08-03 15:02:16 -0300

    commit 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,9 +9,16 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
   title: string = "Gonzalo Alvarez";
-  paragraph: string = "Ingeniero y Doctor en Ingeniería. Coordinador y Desarrollador de Proyectos en diversas ramas de la ingeniería: procesos, productos, optimización, programación en varios lenguajes "+
-   "//  Engineer and Doctor in Engineering. Coordinator and Developer of projects in various branches of engineering: processes, products, optimization, programming in several languages.";
+  paragraph: string = 
+  
+  " Engineer and Doctor in Engineering. Coordinator and Developer of projects in several fields of engineering: processes, products, optimization, programming in several languages"+
 
+  
+
+  "                           //             -----             //                                 "+
+  
+  "Ingeniero y Doctor en Ingeniería. Coordinador y Desarrollador de Proyectos en diversas ramas de la ingeniería: procesos, productos, optimización, programación en varios lenguajes ";
+ 
   ngOnInit() {}
 
 }
